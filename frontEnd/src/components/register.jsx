d84dc2d6d1b6a57beacd385d3b2bd7f70b346055
asnganje <abd.nganje@gmail.com>
2024-01-18 14:34:36 +0300

    add register component content and styling

--- frontEnd/src/components/register.jsx
+++ frontEnd/src/components/register.jsx
@@ -1,31 +1,45 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
     return(
-        <form>
-            <div>
-                <label>Firstname</label>
-                <input 
-                type="text" 
-                />
+        <div className="flex items-center text-xl font-mono justify-center h-screen">
+            <div className="bg-blue-100 p-5 shadow-lg rounded-md w-[70vh]">
+                <form>
+                    <div className="mb-3 flex gap-5">
+                        <label className="mt-1">Firstname</label>
+                        <input 
+                        type="text"
+                        className="rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
+                        />
+                    </div>
+                    <div className="mb-3 flex gap-5">
+                        <label className="mt-1">Lastname</label>
+                        <input 
+                        type="text" 
+                        className="ml-2.5 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300"
+                        />
+                    </div>
+                    <div className="mb-3 flex gap-5">
+                        <label className="mt-1">Email</label>
+                        <input 
+                        type="text"
+                        className="ml-11 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
+                        />
+                    </div>
+                    <div className="mb-3 flex gap-5">
+                        <label className="mt-1">Password</label>
+                        <input 
+                        type="text"
+                        className="ml-2.5 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300" 
+                        />
+                    </div>
+                    <button className="text-white rounded-md w-[50%] mx-[35%] bg-blue-400 hover:bg-blue-500 p-2">Submit</button>
+                </form>
+                <div className="flex gap-5 mt-5">
+                    <p className="text-gray-600 italic space-x-0">Already registered?</p><Link to='/' className="text-gray-600 hover:text-blue-500 hover:underline">Login here!</Link>
+                </div>
             </div>
-            <div>
-                <label>Lastname</label>
-                <input 
-                type="text" 
-                />
             </div>
-            <div>
-                <label>Email</label>
-                <input 
-                type="text" 
-                />
-            </div>
-            <div>
-                <label>Password</label>
-                <input 
-                type="text" 
-                />
-            </div>
-        </form>
     )
 }
 
