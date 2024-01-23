11c65b212129ad98a23fcf73a79c8d9facbe72ae
asnganje <abd.nganje@gmail.com>
2024-01-23 16:00:44 +0300

    add login logic

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,19 +1,51 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { login } from "../redux/thunks/userThunk";
 const Login = () => {
+    const dispatch = useDispatch()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const emailHandler = (e)=> {
+        setEmail(e.target.value)
+    }
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const user = {email, password}
+    
+    const {logged, loggedIn} = useSelector((store)=>store.user)
+    const handleLoginSubmit = (e) => {
+        e.preventDefault()
+        dispatch(login(user))
+        setEmail('')
+        setPassword('')
+    }
+    // console.log(logged);
+    // if (logged.length > 0) {
+    //     return <p>Welcome {logged[0].msg.firstName}</p>
+    // }
     return(
         <div className="flex items-center text-xl font-mono justify-center h-screen">
-            <div className="bg-blue-100 p-5 shadow-lg rounded-md w-[65vh]">
-                <form>
+            {!loggedIn? (<div className="bg-blue-100 p-5 shadow-lg rounded-md w-[65vh]">
+                <form onSubmit={handleLoginSubmit}>
                     <div className="mb-3 flex gap-8">
                         <label className="text-gray-600">Email</label>
                         <input 
-                        type="text" 
+                        type="text"
+                        value={email}
+                        onChange={emailHandler} 
                         className="w-[80%] ml-3 rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300"
                         />
                     </div> 
                     <div className="mb-3 flex gap-3">
                         <label className="text-gray-600">Password</label>
-                        <input type="text" 
+                        <input type="text"
+                        value={password}
+                        onChange={passwordHandler} 
                         className="w-full rounded-md p-2 focus:outline-none focus:ring focus:border-blue-300"
                         />
                     </div> 
@@ -22,7 +54,9 @@ const Login = () => {
                 <div className="flex gap-5 mt-5">
                     <p className="text-gray-600 italic">Not registered?</p><Link to='/register' className="text-gray-600 hover:text-blue-500 hover:underline">click here!</Link>
                 </div>
-            </div>
+            </div>): 
+                (<p className="font-mono" >Welcome {logged[0].msg.firstName}</p>)
+            }
         </div>
     )
 }
