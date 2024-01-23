d95882d01a7db4b7ce6876d6f044f0e31a5c70ef
asnganje <abd.nganje@gmail.com>
2024-01-23 20:34:33 +0300

    modify error message

--- src/redux/thunks/userThunk.js
+++ src/redux/thunks/userThunk.js
@@ -21,7 +21,7 @@ const login = createAsyncThunk('user/login', async (user)=> {
             const response = await axios.post(url, user)    
             return response.data
         } catch (error) {
-            throw new Error(`Error logging in because of ${error}`)            
+            throw new Error(`There is error logging in because of ${error}`)            
         }
 })
 
