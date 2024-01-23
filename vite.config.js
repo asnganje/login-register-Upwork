5863b468a0ab8ce633060dee1a594821f63f5fea
asnganje <abd.nganje@gmail.com>
2024-01-23 16:00:19 +0300

    configure vite to communicate with backend

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
     port: 4173,
   },
   plugins: [react()],
+  proxy: {
+    "/api": {
+      target: "http://localhost:3002",
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, "/api/v1"),
+    },
+  },
 });
