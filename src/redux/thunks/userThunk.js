7947ecf2b02845d577f581c963eca4a18cdd4c14
asnganje <abd.nganje@gmail.com>
2024-01-23 19:24:50 +0300

    update url

--- src/redux/thunks/userThunk.js
+++ src/redux/thunks/userThunk.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios'
 
-const backendUrl = import.meta.env.REACT_APP_BACKEND_URL
+const backendUrl = import.meta.env.REACT_APP_BACKEND_URL;
 
 const createUser = createAsyncThunk('user/createUser', async (user)=> {
         const url = `${backendUrl}/api/v1/auth/register`
